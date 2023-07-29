309fa3d289e50acf03603f844c1530a64a177214
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2023-07-30 03:18:19 +0900

    Lesson 29
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

---
+++ lesson_29/main.hs
@@ -0,0 +1,20 @@
+import Data.List qualified
+
+allFmap :: (Applicative f) => (a -> b) -> f a -> f b
+allFmap = fmap
+
+example :: Int
+example = (*) ((+) 2 4) 6
+
+exampleMaybe :: Maybe Int
+exampleMaybe = pure (*) <*> (pure (+) <*> pure 2 <*> pure 4) <*> pure 6
+
+lastNightPurchase = [6, 12]
+
+drankLastNight = 4
+
+friends = [2, 3]
+
+drinksPerPerson = [3, 4]
+
+neededBeer = Data.List.maximum (((-) <$> ((*) <$> friends <*> drinksPerPerson)) <*> (flip (-) 4 <$> lastNightPurchase))
