4af5d74e28f39bc83973bf9d20a45ed6e7165267
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2023-07-30 20:32:58 +0900

    Lesson 37
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- lesson_35/compare-pizzas/app/Main.hs
+++ lesson_35/compare-pizzas/app/Main.hs
@@ -16,25 +16,3 @@ main = do
   let pizza2 = (read size2, read cost2)
   let betterPizza = comparePizzas pizza1 pizza2
   putStrLn (describePizza betterPizza)
-
-type Pizza = (Double, Double)
-
-costPerInch :: Pizza -> Double
-costPerInch (size, cost) = cost / areaGivenDiameter size
-
-comparePizzas :: Pizza -> Pizza -> Pizza
-comparePizzas p1 p2 =
-  if costP1 < costP2
-    then p1
-    else p2
-  where
-    costP1 = costPerInch p1
-    costP2 = costPerInch p2
-
-describePizza :: Pizza -> String
-describePizza (size, cost) = "The " ++ show size ++ " pizza " ++ "is cheaper at " ++ show costSqInch ++ " per square inch"
-  where
-    costSqInch = costPerInch (size, cost)
-
-areaGivenDiameter :: Double -> Double
-areaGivenDiameter size = pi * ((size / 2) ^ 2)
